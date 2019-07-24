8f895adf120b6e94757d4abc47a108bfa8419e2f
Samir Saad <samir.saadallah@gmail.com>
2019-07-23 22:20:29 -0400

    test

--- vars/customize.groovy
+++ vars/customize.groovy
@@ -1,39 +1,36 @@
 #!/usr/bin/groovy
+import com.cloudbees.groovy.cps.NonCPS
 import groovy.json.JsonSlurper
 import org.ssaad.ami.pipeline.common.Pipeline
-import org.ssaad.ami.pipeline.stage.Stage
 
+@NonCPS
 def call(Pipeline myPipeline) {
 
     println("Customize pipeline")
 
-    for (Stage currentStage : myPipeline.stages) {
-        println("Current stage is: ${currentStage.name}")
-
-        String config =
-                "{\n" +
-                        "    \"stages\": [        \n" +
-                        "        {\n" +
-                        "            \"confirmation\": {\n" +
-                        "                \"time\": 10,\n" +
-                        "                \"enable\": true\n" +
-                        "            },\n" +
-                        "            \"taskType\": \"CODE_BUILD\",\n" +
-                        "            \"activation\": {\n" +
-                        "                \"allowedBranches\": [\n" +
-                        "                    \"ANY\",\n" +
-                        "                    \"FEATURE\"\n" +
-                        "                ],\n" +
-                        "                \"allowedAppType\": [\n" +
-                        "                    \"ANY\",\n" +
-                        "                    \"LIBRARY\"\n" +
-                        "                ]\n" +
-                        "            }\n" +
-                        "        }\n" +
-                        "    ]\n" +
-                        "}"
-        // using Map to convert to Person object type
-        myPipeline.customize(new JsonSlurper().parseText(config))
-        myPipeline.print()
-    }
+    String config =
+            "{\n" +
+                    "    \"stages\": [        \n" +
+                    "        {\n" +
+                    "            \"confirmation\": {\n" +
+                    "                \"time\": 10,\n" +
+                    "                \"enable\": true\n" +
+                    "            },\n" +
+                    "            \"taskType\": \"CODE_BUILD\",\n" +
+                    "            \"activation\": {\n" +
+                    "                \"allowedBranches\": [\n" +
+                    "                    \"ANY\",\n" +
+                    "                    \"FEATURE\"\n" +
+                    "                ],\n" +
+                    "                \"allowedAppType\": [\n" +
+                    "                    \"ANY\",\n" +
+                    "                    \"LIBRARY\"\n" +
+                    "                ]\n" +
+                    "            }\n" +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}"
+    // using Map to convert to Person object type
+    myPipeline.customize(new JsonSlurper().parseText(config))
+    myPipeline.print()
 }
