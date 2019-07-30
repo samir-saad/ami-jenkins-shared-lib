91cd6bdf5eb3290fe632e47f5bc7a21efdbf8c67
Samir Saad <samir.saadallah@gmail.com>
2019-07-30 13:17:11 -0400

    test03

--- src/org/ssaad/ami/pipeline/common/PipelineFactory.groovy
+++ src/org/ssaad/ami/pipeline/common/PipelineFactory.groovy
@@ -6,14 +6,23 @@ import org.ssaad.ami.pipeline.stage.StageInitialization
 class PipelineFactory {
 
     Pipeline create(PipelineInitialization init) {
+        init.steps.println("Pipeline Factory")
 
         Pipeline pipeline = new Pipeline()
         switch (init.pipelineType) {
             case PipelineType.SPRING_MAVEN_OPENSHIFT:
+                init.steps.println("SPRING_MAVEN_OPENSHIFT")
                 pipeline.id = "maven-spring-ocp-pipeline"
                 pipeline.name = "maven-spring-ocp-pipeline"
 
-                init.addStageInit(StageInitialization.create(TaskType.CODE_BUILD, EngineType.MAVEN, null))
+                init.steps.println("Create stage init CODE_BUILD")
+                StageInitialization stageInit = StageInitialization.create(TaskType.CODE_BUILD, EngineType.MAVEN, null)
+                init.steps.println("Created stage init " + stageInit.toString())
+                init.steps.println("Add stage init ")
+                init.addStageInit(stageInit)
+                init.steps.println("Added stage init ")
+
+
                 init.addStageInit(StageInitialization.create(TaskType.UNIT_TESTS, EngineType.MAVEN, null))
                 init.addStageInit(StageInitialization.create(TaskType.BINARIES_ARCHIVE, EngineType.MAVEN, null))
 
