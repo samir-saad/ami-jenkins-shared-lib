5ce75b17fd6ea1b1c67229821035cac9a5b6bb4b
Samir Saad <samir.saadallah@gmail.com>
2019-07-30 12:42:17 -0400

    fix

--- src/org/ssaad/ami/pipeline/common/PipelineFactory.groovy
+++ src/org/ssaad/ami/pipeline/common/PipelineFactory.groovy
@@ -1,16 +1,7 @@
 package org.ssaad.ami.pipeline.common
 
-import org.ssaad.ami.pipeline.common.types.AppRuntimeType
-import org.ssaad.ami.pipeline.common.types.DeploymentType
-import org.ssaad.ami.pipeline.common.types.EngineType
-import org.ssaad.ami.pipeline.common.types.EnvironmentType
-import org.ssaad.ami.pipeline.common.types.PipelineType
-import org.ssaad.ami.pipeline.common.types.PlatformType
-import org.ssaad.ami.pipeline.common.types.PluginType
-import org.ssaad.ami.pipeline.common.types.TaskType
-import org.ssaad.ami.pipeline.common.types.TemplateType
+import org.ssaad.ami.pipeline.common.types.*
 import org.ssaad.ami.pipeline.stage.StageInitialization
-import org.ssaad.ami.pipeline.utils.TemplateUtils
 
 class PipelineFactory {
 
@@ -24,7 +15,7 @@ class PipelineFactory {
 
                 init.addStageInit(StageInitialization.create(TaskType.CODE_BUILD, EngineType.MAVEN))
                 init.addStageInit(StageInitialization.create(TaskType.UNIT_TESTS, EngineType.MAVEN))
-                init.addStageInit(StageInitialization.create(TaskType.BINARIES_ARCHIVE, EngineType.MAVEN,))
+                init.addStageInit(StageInitialization.create(TaskType.BINARIES_ARCHIVE, EngineType.MAVEN))
 
                 init.addStageInit(StageInitialization.create(TaskType.CONTAINER_BUILD, EngineType.OPENSHIFT, PluginType.OPENSHIFT_S2I,
                         PlatformType.OPENSHIFT, EnvironmentType.DEV, AppRuntimeType.JDK, DeploymentType.RECREATE, TemplateType.S2I_BUILD))
