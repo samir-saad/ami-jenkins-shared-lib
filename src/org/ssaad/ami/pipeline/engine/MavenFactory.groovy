c0c2a1e71e94355db6c673e42edadb7fd47f3221
Samir Saad <samir.saadallah@gmail.com>
2020-05-31 23:29:21 -0400

    Fix sonar params

--- src/org/ssaad/ami/pipeline/engine/MavenFactory.groovy
+++ src/org/ssaad/ami/pipeline/engine/MavenFactory.groovy
@@ -27,8 +27,8 @@ class MavenFactory {
                 if (PluginType.MAVEN_SONAR_SCAN.equals(init.pluginType)) {
                     maven.configItemId = "sonarqube"
                     maven.goals = "sonar:sonar"
-                    maven.params = "-Dsonar.projectKey=${app.group}:${app.id}:${app.branch} " +
-                            "-Dsonar.projectName=${app.name} (${app.branch})" +
+                    maven.params = "-Dsonar.projectKey=\${app.group}:\${app.id}:\${app.branch} " +
+                            "-Dsonar.projectName=\${app.name} (\${app.branch}) " +
                             "-DskipTests=true"
 //                    maven.params = "-DskipTests=true -Dsonar.branch=\${app.branch}"
                 }
