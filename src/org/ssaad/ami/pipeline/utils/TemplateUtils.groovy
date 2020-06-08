314e759055490730925edc807263833794a97f2e
Samir Saad <samir.saadallah@gmail.com>
2020-06-08 14:12:05 -0400

    push to quay

--- src/org/ssaad/ami/pipeline/utils/TemplateUtils.groovy
+++ src/org/ssaad/ami/pipeline/utils/TemplateUtils.groovy
@@ -137,7 +137,8 @@ class TemplateUtils implements Serializable {
         template.filePath = "/build/s2i/build-template.yaml"
         template.params = getCommonParams()
         template.params.put("BUILD_NAME", '${app.id}')
-        template.params.put("IMAGE_NAME", 'docker.io/samirsaad/${app.id}')
+//        template.params.put("IMAGE_NAME", 'docker.io/samirsaad/${app.id}')
+        template.params.put("IMAGE_NAME", 'quay.io/samir.k.saad/${app.id}')
         template.params.put("IMAGE_TAG", '${app.version}')
         template.params.put("INPUT_STREAM_NAME", '${engine.imageStream.name}')
         template.params.put("INPUT_STREAM_TAG", '${engine.imageStream.tag}')
